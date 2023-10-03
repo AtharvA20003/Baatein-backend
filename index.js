fa042c3d49dac3246fd8ea0f51857db0f089eff6
AtharvA20003 <111067071+AtharvA20003@users.noreply.github.com>
2023-10-03 17:40:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
 // Node server handling socket.io connection
-const express = require('express');
-const http = require('http');
-const PORT =process.env.PORT || 8000;
-const { Server } = require("socket.io");
-const path = require('path');
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
+const PORT = process.env.PORT || 8000;
+const io = require('socket.io')(PORT , {
     cors:{
         origin:'*'
     }
 });
-
-app.use(express.static(path.join(__dirname, 'pages')));
-// const io = require('socket.io')(PORT, {
-//     cors:{
-//         origin:'*'
-//     }
-// });
-
-
+console.log(PORT);
 const users = {};
 io.on('connection', socket=>{                        //listens to all different socket connections; like different people joining.
     socket.on('new-user-joined', Name=>{            //listens and handles on what should happen with one particular connection.
         users[socket.id] = Name;                    //as someone joined, make sokcet-id=name
         socket.broadcast.emit('user-joined',Name);  //Tell to all the people already joined, that a new user joined.
     });
-
+    console.log('First one executed');
     socket.on('send', message =>{
         socket.broadcast.emit('recieve',{message:message, name:users[socket.id]}); //send the message to everyone
     });
-
+    console.log('Second one executed');
     socket.on('disconnect', message =>{
         socket.broadcast.emit('left', users[socket.id]); 
         delete users[socket.id];
     }); 
-    server.listen(PORT, ()=>{
-        console.log(`listening on ${PORT}`)
-    })
-    app.listen(PORT, ()=>{
-        console.log(`listening on ${PORT}`);
-    })
-})
+    console.log('Third one executed');
+    
+});
+
+
